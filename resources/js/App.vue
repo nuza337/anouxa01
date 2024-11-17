4e13d278c343dbb52a6dfbad6e1210513aacca3e
nuza337 <nuza337123@gmail.com>
2024-11-17 19:22:43 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+   <div>
+    <h1>hello</h1>
+   </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'anouxaApp',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+
+</script>
+
+<style iang="scss" scoped>
+
+</style>
